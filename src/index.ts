bd3afa4fc54ffec59f395e0a038adc052b17b727
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-02-28 11:13:38 -0300

    tipos en variables, arrays y objetos

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-let mensaje : String
+//notacion literal de variables
+let mensaje: string
 mensaje = 'Hola Mundo'
+
+let numero: number
+numero = 123
+
+//inferencia de tipo de dato
+let mensaje2 = 'Hola Mundo 2'
+// mensaje2 = 123
+
+//tipo 123
+const numero2 = 123
+
+class Persona{}
+
+//tipo de dato Persona
+let persona = new Persona()
+
+//tipos de arreglos
+const arreglo: number[] = [1, 2, 3, 4, 5]
+const arreglo2: Array<number> = [1, 2, 3, 4, 5]
+
+//objeto
+interface Usuario{
+    nombre: string
+}
+
+const usuarioArray: Usuario[] = [{nombre: 'Juan'}, /*12*/]
